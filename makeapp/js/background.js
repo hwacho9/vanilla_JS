c6733014c5d02c2e8c6f4614f9cd62e6254a4ba5
hwacho9 <63436365+hwacho9@users.noreply.github.com>
2023-03-02 23:54:33 +0900

    backgroud

---
+++ makeapp/js/background.js
@@ -0,0 +1,14 @@
+const images = [
+    "0.jpg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+]
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img"); //html 에 img 생성
+
+bgImage.src = `img/${chosenImage}`; // 이미지를 선택하고 src로 img 폴더 뒤에 추가
+
+document.body.appendChild(bgImage) //bgImage를 body 에 추가 append 는 가장 뒤에 prepend 는 가장 위에 오게한다
