560c3e0d7a1de8aabdf5e6d71b63a6214af6e8c6
hwacho9 <csh77776@gmail.com>
2023-03-05 16:36:06 +0900

    synchromnization

--- makeapp/js/weather.js
+++ makeapp/js/weather.js
@@ -1,11 +1,19 @@
+const API_KEY = "d437a794b2b3ea71301453062adb4a81";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError() {
     alert("Can't find you. No weather for you.")
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
-//여기까지 현재 위치 파악
+//여기까지 현재 위치 파악 
